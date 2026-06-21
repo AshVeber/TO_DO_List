8a3af793c5fabf0d3ff7ff5f432ba36415eb6714
AshVeber <ashveber42@gmail.com>
2026-06-21 10:03:34 +0300

    add cmake

---
+++ build/CMakeFiles/TODO.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TODO.dir/link.d"
+  "CMakeFiles/TODO.dir/src/to_do_list.cpp.o"
+  "CMakeFiles/TODO.dir/src/to_do_list.cpp.o.d"
+  "TODO"
+  "TODO.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TODO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/TODO.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for TODO.
+# This may be replaced when dependencies are built.
